f08b714a20c4d1e4f51c62fbf67fb8c052fabe95
KatePaulauskas <>
2024-01-02 12:22:22 +0000

    Add JavaScript code to ensure DOM is fully loaded before any functions run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-// Loop through buttons to locate buttons with "new-entry" class, add event listener and display collapsable areas when buttons are clicked"
+// Ensure DOM is fully loaded before running Trading Tracker functions
 
-const allNewEntryButtons = document.getElementsByClassName("new-entry");
-for (let button of allNewEntryButtons) {
-    button.addEventListener("click", function () {
-        let collapsableEntryArea = this.nextElementSibling;
-        let collapsableEntryAreaIsHidden = collapsableEntryArea.style.display === 'none';
-        collapsableEntryArea.style.display = collapsableEntryAreaIsHidden ? "block" : "none";
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Loop through buttons to locate buttons with "new-entry" class, add event listener and display collapsable areas when buttons are clicked"
+
+    const allNewEntryButtons = document.getElementsByClassName("new-entry");
+    for (let button of allNewEntryButtons) {
+        button.addEventListener("click", function () {
+            let collapsableEntryArea = this.nextElementSibling;
+            let collapsableEntryAreaIsHidden = collapsableEntryArea.style.display === 'none';
+            collapsableEntryArea.style.display = collapsableEntryAreaIsHidden ? "block" : "none";
+            });
+    }
+});
 
 
 /**
 * Get the initial investemnt amount, ensure the user input is a positive number
-* Aletr the user if input is not a positive number
+* Alert the user if input is not a positive number
 */
 
 // Update the local storage balance
@@ -20,7 +25,8 @@ for (let button of allNewEntryButtons) {
 // Display the invested amount
 
 /**
- * Adding or withdrawing form the invested amount
+ * Adding or withdrawing from the invested amount
  * If a user wants to update invested amount, get the new number, add or subtract it to / from the existing balance
 * Update displayed balance
 */
+

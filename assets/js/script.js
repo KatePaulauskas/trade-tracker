e5133be4e74aec76a94add33321a7fde6bf51ba3
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-01-13 17:57:45 +0000

    Remove the 'Withdraw' button from the 'Investment section'. Remove any CSS and JavaScript associated with the 'Withdraw' button. Replace the word 'balance' with the word 'investment', used for attributes in the 'Investment' section, to improve readability of the code and avoid confusion with the 'Current balance' field in the summary table. Update the rest of the code to reflect the change. Rename the function that handles the addition of the investment, to improve readability of the code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,43 +26,30 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Add event listeners to 'Add' and 'Withdraw' buttons
-document.getElementById("add-balance").addEventListener("click", function() {
-	investmentIsPositiveAmount("add");
-});
-
-document.getElementById("withdraw-balance").addEventListener("click", function() {
-	investmentIsPositiveAmount("withdraw");
+document.getElementById("add-investment").addEventListener("click", function() {
+	addInvestment();
 });
 
 // Get the investment amount entred by a user
 function getInvestmentAmount() {
-	return parseFloat(document.getElementById("balance-box").value);
+	return parseFloat(document.getElementById("investment-box").value);
 }
 
-// Ensure the user input is a positive amount, if so, update the local storage balance
-function investmentIsPositiveAmount(action) {
+// Ensure the user input is a positive amount, if so, update the local storage investment amount
+function addInvestment() {
+
 	// Check if the form is valid. Source: https://developer.mozilla.org/en-US/docs/Web/HTML/Constraint_validation#constraint_validation_process
-	if (document.getElementById("balance-form").checkValidity()) {
+	if (document.getElementById("investment-form").checkValidity()) {
+
 		// Form is valid, proceed with the existing logic
 		let amount = getInvestmentAmount();
+
 		// Get stored investment from the local storage, or display 'zero' if no data stored
 		let currentInvestment = parseFloat(localStorage.getItem("investmentResult")) || 0;
-
 		if (amount > 0) {
-			if (action === "add") {
-				// Add the entered amount to the invested balance
+				// Add the entered amount to already invested amount
 				currentInvestment += amount;
-			} else if (action === "withdraw") {
-
-				// Subtract the amount from the invested balance
-				if (currentInvestment >= amount) {
-					currentInvestment -= amount;
-				} else {
-					alert("Cannot withdraw more than the currently invested amount!");
-					return;
-				}
-			}
-			storeBalance(currentInvestment);
+			storeInvestment(currentInvestment);
 		}
 		// Alert the user if input is not a positive number
 		else {
@@ -70,21 +57,21 @@ function investmentIsPositiveAmount(action) {
 		}
 	} else {
 		// Form is not valid, show an error message. Source: https://developer.mozilla.org/en-US/docs/Web/HTML/Constraint_validation#constraint_validation_process        
-		document.getElementById("balance-box").reportValidity();
+		document.getElementById("investment-box").reportValidity();
 	}
 	updateCurrentBalanceAndProfitLossPercent();
 };
 
 
-// Function to update the local storage balance. Source: https://www.freecodecamp.org/news/web-storage-localstorage-vs-sessionstorage-in-javascript/
-function storeBalance(amount) {
+// Function to update the local storage invested amount. Source: https://www.freecodecamp.org/news/web-storage-localstorage-vs-sessionstorage-in-javascript/
+function storeInvestment(amount) {
 	localStorage.setItem("investmentResult", amount);
 
 	// Call the function that displays the invested amount to reflect each investment update recorded in the local storage
 	updateDisplayedInvestment();
 
-	// Call the function to clear the balance box once the investment amount is stored and displayed 
-	clearBalanceBox();
+	// Call the function to clear the investment box once the investment amount is stored and displayed 
+	clearInvestmentBox();
 };
 
 
@@ -94,9 +81,9 @@ function updateDisplayedInvestment() {
 	document.getElementById("total-investment").innerText = currentInvestment;
 };
 
-// Clear balance box after the invested amount is stored to the local storage
-function clearBalanceBox() {
-	document.getElementById("balance-box").value = '';
+// Clear investment box after the invested amount is stored to the local storage
+function clearInvestmentBox() {
+	document.getElementById("investment-box").value = '';
 };
 
 // Add an event listener to the 'Submit' button in the 'Add Trades' section
@@ -114,10 +101,10 @@ function calculateTotalProfitLossAmount() {
     // Check if the form is valid
     if (document.getElementById("add-trade").checkValidity()) {
         if (document.getElementById("total-investment").innerText === '0') {
-            alert("First enter balance! Cannot add trades without having a positive balance!");
+            alert("First enter investment! Cannot add trades without having an investment to trade!");
             return;
         } else {
-            // If form is valid and balance is entered, proceed with the following logic
+            // If form is valid and investment is entered, proceed with the following logic
             let enteredProfitLossAmount = getProfitLossAmount();
             let currentProfitLossAmount = parseFloat(localStorage.getItem("tradingResult")) || 0;
             if (enteredProfitLossAmount < 0) {
